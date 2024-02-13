ba32d1c90976b20cc0661a299ecd88aad33275bb
Jetracon <vovalucky80849@gmail.com>
2024-02-13 15:48:18 +0200

    new

--- asyncdb/queries.py
+++ asyncdb/queries.py
@@ -1,4 +1,6 @@
-from sqlalchemy import select
+from typing import Optional
+
+from sqlalchemy import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from asyncdb.models import ProductModel
@@ -11,3 +13,20 @@ async def get_products(db: AsyncSession, offset: int, limit: int):
         products = result.scalars().all()
 
         return products
+
+async def search_product(db: AsyncSession, product_id: int) -> Optional[ProductModel]:
+    async with db as session:
+        result = await session.execute(
+            select(ProductModel)
+            .where(ProductModel.id == product_id)
+        )
+        return result.scalar_one_or_none()
+
+async def delete_product(db: AsyncSession, product_id: int) -> bool:
+    async with db as session:
+        result = await session.execute(
+            delete(ProductModel)
+            .where(ProductModel.id == product_id)
+        )
+        await session.commit()
+        return bool(result.rowcount)
